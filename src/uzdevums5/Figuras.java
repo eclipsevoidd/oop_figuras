9bf6af37c328911c55c5bb48ce3b40a872dd7b10
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-09-23 12:19:06 +0300

    Izvēlnes sagataves izveidošana

--- src/uzdevums5/Figuras.java
+++ src/uzdevums5/Figuras.java
@@ -13,7 +13,13 @@ public class Figuras {
 		Cetrsturis cetrsturis1 = new Cetrsturis();
 		System.out.println(cetrsturis1.izvaditCetrsturaInfo());
 		// izvadīt šī četrstūra atrašanās vietu
-		centraP1.parvietot(5, -10);
 		System.out.println(cetrsturis1.punkts.izvaditKoordinatas());
+		
+		Cetrsturis cetrsturis2 = new Cetrsturis(centraP1, 5, 11);
+		System.out.println(cetrsturis2.izvaditCetrsturaInfo() + "\n" + cetrsturis2.punkts.izvaditKoordinatas() + "\ns = " + cetrsturis2.LaukumaAprekinasana());
+		
+		Centrs punkts2 = new Centrs(5, 23);
+		Aplis aplis1 = new Aplis(punkts2, 5);
+		System.out.println(aplis1.izvaditAplaInfo() + "\ns = " + aplis1.rLaukums() + "\nc = " + aplis1.rLinijasGarums());
 	}	
 }
