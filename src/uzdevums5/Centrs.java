3fa68b420057a7dbf5d6281e2dcac2222697ea51
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-09-23 11:20:49 +0300

    Pabeigta klase Punkts

---
+++ src/uzdevums5/Centrs.java
@@ -0,0 +1,22 @@
+package uzdevums5;
+
+public class Centrs {
+	// Atribūti
+	public int x, y;
+	
+	// Konstruktors (nosaukums tāds pats kā klasei)
+	public Centrs (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	// Metodes
+	public String izvaditKoordinatas() {
+		return "Centra punkta koordinātas: \n x = " + x + "y = " + y;
+	}
+	
+	public void parvietot(int koordX, int koordY) {
+		x += koordX;
+		y += koordY;
+	}
+}
