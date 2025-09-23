9f9c5254caffd5717056a4c874b46d5bf67aae0a
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-09-23 11:35:18 +0300

    Pabeigta klase Cetrsturis

--- src/uzdevums5/Cetrsturis.java
+++ src/uzdevums5/Cetrsturis.java
@@ -1,5 +1,27 @@
 package uzdevums5;
 
 public class Cetrsturis {
-
+	public double platums, augstums;
+	public Centrs punkts;
+	
+	// Konstruktora pārslogošana (overloading)
+	public Cetrsturis() {
+		punkts = new Centrs(0, 0);
+	}
+	
+	public Cetrsturis(Centrs punkts, double p, double a) {
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+	}
+	
+	public String izvaditCetrsturaInfo() {
+		
+		return "Platums: " + platums + " cm\n Augstums: " + augstums + " cm";
+	}
+	
+	// Uztaisīt metodi, kas aprēķina laukumu
+	public double LaukumaAprekinasana() {
+		return platums * augstums;
+	}
 }
